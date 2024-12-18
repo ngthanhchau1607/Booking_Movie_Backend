ca8dd8a2f92d67ec1b8eb8bed23b9b846ea7a69a
chau03 <thanhchau16072003@gmail.com>
2024-12-18 15:11:39 +0700

    update

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,10 +1,14 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../cinema-book-5d937-firebase-adminsdk-j5wi7-9f67da28b5.json");
+var serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Thay thế các ký tự '\n' thành ký tự dòng mới thực tế
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://cinema-book-5d937.appspot.com",
+  storageBucket: `gs://${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
 });
 
 const bucket = admin.storage().bucket();
