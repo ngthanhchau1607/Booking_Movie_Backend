cc9273c21a3c4fc6f6385426cac5c6dd18605951
chau03 <thanhchau16072003@gmail.com>
2024-12-18 15:48:45 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 5000;
+
+// Sử dụng port từ biến môi trường Render
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-  console.log(process.env.HOSTNAME);
 });
